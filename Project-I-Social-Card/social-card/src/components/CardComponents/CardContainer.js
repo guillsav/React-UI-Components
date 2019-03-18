a3f26ff8f3166e0afa4efe1657778846899a684b
guillsav <guillsav@gmail.com>
2019-03-18 17:08:11 -0400

    MVP done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import CardBanner from './CardBanner';
 import CardContent from './CardContent';
+
 import './Card.css';
 
 const CardContainer = () => {
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = () => {
-  return <div className="image-thumbnail" />;
+  return (
+    <div className="image-thumbnail">
+      <img
+        src={require('./lambda-school-logo-red.jpg')}
+        alt="lambda school logo"
+      />
+    </div>
+  );
 };
 
 export default ImageThumbnail;
