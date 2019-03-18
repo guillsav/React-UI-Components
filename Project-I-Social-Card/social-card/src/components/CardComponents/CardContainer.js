e11e1538bd89ae0f041bb23dd49194449663a307
guillsav <guillsav@gmail.com>
2019-03-18 16:43:51 -0400

    react component build

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 import './Card.css';
+
+const CardContainer = () => {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <CardBanner />
+        <CardContent />
+      </div>
+    </div>
+  );
+};
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className="title">
+      <h4>Lambda School</h4>
+      <p>@lambdaschool · 26 jan</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
