5ac40ecbf2840f97767d34e002c1d088e751abf2
guillsav <guillsav@gmail.com>
2019-03-18 22:15:55 -0400

    background-size 150%

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
       <div className="App">
         <HeaderContainer />
         <CardContainer />
-        <Footer />
+        <Footer test="Hello" />
       </div>
     </div>
   );
